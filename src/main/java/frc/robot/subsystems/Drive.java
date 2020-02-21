8faa82be173a063c91e134eeed2eea01f2f9c5b3
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2020-02-21 23:35:13 +0200

    Updated comments in Shooter, removed extraneous import

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.constants.Constants;
 
