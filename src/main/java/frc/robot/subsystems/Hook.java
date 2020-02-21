1d5091199e0a504107470253aeba3c1b13fc9d4c
Sebastian Serrano <sebastianserrano@Sebastians-MacBook-Air.local>
2020-02-21 16:12:22 +0200

    added comments

--- src/main/java/frc/robot/subsystems/Hook.java
+++ src/main/java/frc/robot/subsystems/Hook.java
@@ -7,10 +7,17 @@ public class Hook{
 
     private CANSparkMax hookMotor;
 
+    /**
+     * 
+     * @param hook - motor for hook
+     */
     public Hook(CANSparkMax hook) {
         this.hookMotor = hook;
     }
-
+    /**
+     * 
+     * @param speed - motor speed
+     */
     public void setSpeed (double speed){
         hookMotor.set(speed);
     }
