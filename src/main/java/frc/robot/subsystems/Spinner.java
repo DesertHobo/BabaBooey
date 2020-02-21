5d215f1b3ff7cbd0616d57962a54f6ce82a8ca42
Tyler4338 <u21tcrawford@wbais.net>
2020-02-21 17:37:24 +0200

    Added Spinner

---
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+public class Spinner {
+
+    // motor for moving hook left/right
+    private VictorSPX SpinnerMotor;
+
+    public Spinner() {
+        
+    }
+
+    public void setSpeed (){
+       
+    }
+
+}
